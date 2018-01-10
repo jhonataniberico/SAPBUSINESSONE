23ddc4d78666297e5b01d68f32094cfff9fc435a
joseminaya15 <jose.minayac15@gmail.com>
2018-01-10 18:02:01 -0500

    bootstrap-select

--- application/views/v_es.php
+++ application/views/v_es.php
@@ -715,7 +715,11 @@
     <script src="<?php echo RUTA_JS?>Utils.js?v=<?php echo time();?>"></script>
     <script src="<?php echo RUTA_JS?>index.js?v=<?php echo time();?>"></script>
     <script type="text/javascript">
-  
+    	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+        	$('select').selectpicker('mobile');
+        } else {
+            $('select').selectpicker();
+        }
     </script>
 </body>
 </html>
