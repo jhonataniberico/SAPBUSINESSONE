0c3f4bded5892c8a8171a4d1e790b0d540ee5487
joseminaya15 <jose.minayac15@gmail.com>
2018-01-30 18:49:14 -0500

    vista main

--- application/views/v_admin.php
+++ application/views/v_admin.php
@@ -15,44 +15,49 @@
     <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>toaster/toastr.min.css?v=<?php echo time();?>">
     <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>bootstrap-select/css/bootstrap-select.min.css?v=<?php echo time();?>">
     <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>bootstrap/bootstrap.min.css?v=<?php echo time();?>">
-    <link rel="stylesheet"    href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
-    <link rel="stylesheet"    href="https://cdn.datatables.net/buttons/1.5.1/css/buttons.dataTables.min.css">
     <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>mdl/material.min.css?v=<?php echo time();?>">
     <link rel="stylesheet"    href="<?php echo RUTA_FONTS?>font-awesome.min.css?v=<?php echo time();?>">
     <link rel="stylesheet"    href="<?php echo RUTA_FONTS?>material-icons.css?v=<?php echo time();?>">
     <link rel="stylesheet"    href="<?php echo RUTA_FONTS?>roboto.css?v=<?php echo time();?>">
-	<link rel="stylesheet"      href="<?php echo RUTA_CSS?>m-p.min.css?v=<?php echo time();?>">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.4.2/css/buttons.dataTables.min.css">
-
-</head>
+    <link rel="stylesheet"    href="<?php echo RUTA_CSS?>m-p.min.css?v=<?php echo time();?>">
+    <link rel="stylesheet"    href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
+    <link rel="stylesheet"    href="https://cdn.datatables.net/autofill/2.2.2/css/autoFill.dataTables.min.css">
+    <link rel="stylesheet"    href="https://cdn.datatables.net/buttons/1.5.1/css/buttons.dataTables.min.css">
+    <link rel="stylesheet"    href="https://cdn.datatables.net/colreorder/1.4.1/css/colReorder.dataTables.min.cs">
+    <link rel="stylesheet"    href="https://cdn.datatables.net/fixedcolumns/3.2.4/css/fixedColumns.dataTables.min.css">
+    <link rel="stylesheet"    href="https://cdn.datatables.net/fixedheader/3.1.3/css/fixedHeader.dataTables.min.css">
+    <link rel="stylesheet"    href="https://cdn.datatables.net/responsive/2.2.1/css/responsive.dataTables.min.css">
+    <link rel="stylesheet"    href="<?php echo RUTA_CSS?>index.css?v=<?php echo time();?>">
 <body>
     <section id="principal">
-        <div id="content">
-            <a onclick="cerrarCesion()">Cerrar cesión</a>
-            <div class="table-responsive">
-                <table id="example" class="display nowrap" cellspacing="0" width="100%">
-                  <thead>
-                    <tr class="tr-header-reporte">
-                      <th class="text-center">Nombres</th>
-                      <th class="text-center">Empresa</th>
-                      <th class="text-center">Correo electr&oacute;nico</th>
-                      <th class="text-center">Tel&eacute;fono</th>
-                      <th class="text-center">Industria</th>
-                      <th class="text-center">Cantidad de empleados</th>
-                      <th class="text-center">Facturaci&oacute;n</th>
-                      <th class="text-center">Prioridad</th>
-                      <th class="text-center">Infraestructura</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                      <?php echo $html ?>  
-                  </tbody>
-                </table>
-              </div>
+        <div id="content" class="mdl-container">
+            <img class="logo-home logo-table" src="<?php echo RUTA_IMG?>logo/logo_header.png">
+            <a class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect button-logout" onclick="cerrarCesion()">Cerrar Sesión</a>
+            <div class="mdl-card mdl-card-table">
+                <div class="table-responsive">
+                    <table id="example" class="display nowrap table table-bordered table-hover dt-responsive" cellspacing="0" width="100%">
+                        <thead>
+                            <tr class="tr-header-reporte">
+                                <th class="text-center">Nombres</th>
+                                <th class="text-center">Empresa</th>
+                                <th class="text-center">Correo electr&oacute;nico</th>
+                                <th class="text-center">Tel&eacute;fono</th>
+                                <th class="text-center">Industria</th>
+                                <th class="text-center">Cantidad de empleados</th>
+                                <th class="text-center">Facturaci&oacute;n</th>
+                                <th class="text-center">Prioridad</th>
+                                <th class="text-center">Infraestructura</th>
+                            </tr>
+                        </thead>
+                      <tbody>
+                          <?php echo $html ?>  
+                      </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </section>
-     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/wnumb/1.1.0/wNumb.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/easy-autocomplete/1.3.5/jquery.easy-autocomplete.min.js"></script> 
@@ -61,28 +66,30 @@
     <script src="<?php echo RUTA_PLUGINS?>bootstrap-select/js/i18n/defaults-es_ES.min.js?v=<?php echo time();?>"></script>
     <script src="<?php echo RUTA_PLUGINS?>toaster/toastr.min.js?v=<?php echo time();?>"></script>
     <script type="text/javascript" src="<?php echo RUTA_PLUGINS?>mdl/material.min.js?v=<?php echo time();?>"></script>
-
-    <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.4.2/js/dataTables.buttons.min.js"></script>
-    <script type="text/javascript" language="javascript" src="//cdn.datatables.net/buttons/1.4.2/js/buttons.flash.min.js"></script>
-    <script type="text/javascript" language="javascript" src="//cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script type="text/javascript" language="javascript" src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
-    <script type="text/javascript" language="javascript" src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
-    <script type="text/javascript" language="javascript" src="//cdn.datatables.net/buttons/1.4.2/js/buttons.html5.min.js"></script>
-    <script type="text/javascript" language="javascript" src="//cdn.datatables.net/buttons/1.4.2/js/buttons.print.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.4.2/js/dataTables.buttons.min.js"></script>
+    <script type="text/javascript" src="//cdn.datatables.net/buttons/1.4.2/js/buttons.flash.min.js"></script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
+    <script type="text/javascript" src="//cdn.datatables.net/buttons/1.4.2/js/buttons.html5.min.js"></script>
+    <script type="text/javascript" src="//cdn.datatables.net/buttons/1.4.2/js/buttons.print.min.js"></script>
     <script src="<?php echo RUTA_JS?>index.js?v=<?php echo time();?>"></script>
-
     <script type="text/javascript">
-        $(document).ready(function() {
-            $('#example').DataTable( {
-                dom: 'Bfrtip',
-                buttons: [
-                    'csv', 'excel', 'print'
-                ]
-            } );
-        } );
-
+      $(document).ready(function() {
+          $('#example').DataTable( {
+              responsive: true,
+              dom: 'Bfrtip',
+              buttons: [
+                'excel', 'print'
+              ]
+          });
+          $('.buttons-excel').empty();
+          $('.buttons-print').empty();
+          $('.buttons-excel').append('<i class="fa fa-file-excel-o"></i>');
+          $('.buttons-print').append('<i class="fa fa-print"></i>');
+      });
     </script>
 </body>
 </html>
