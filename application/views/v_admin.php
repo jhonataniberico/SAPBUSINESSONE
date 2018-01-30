be3f24e852bc365c3edd3ee19eb28e6502417fdf
joseminaya15 <jose.minayac15@gmail.com>
2018-01-30 18:53:54 -0500

    error en vista

--- application/views/v_admin.php
+++ application/views/v_admin.php
@@ -23,7 +23,6 @@
     <link rel="stylesheet"    href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
     <link rel="stylesheet"    href="https://cdn.datatables.net/autofill/2.2.2/css/autoFill.dataTables.min.css">
     <link rel="stylesheet"    href="https://cdn.datatables.net/buttons/1.5.1/css/buttons.dataTables.min.css">
-    <link rel="stylesheet"    href="https://cdn.datatables.net/colreorder/1.4.1/css/colReorder.dataTables.min.cs">
     <link rel="stylesheet"    href="https://cdn.datatables.net/fixedcolumns/3.2.4/css/fixedColumns.dataTables.min.css">
     <link rel="stylesheet"    href="https://cdn.datatables.net/fixedheader/3.1.3/css/fixedHeader.dataTables.min.css">
     <link rel="stylesheet"    href="https://cdn.datatables.net/responsive/2.2.1/css/responsive.dataTables.min.css">
