1644a81a144ed2f230c7fa634bd01699d54d0c8f
joseminaya15 <jose.minayac15@gmail.com>
2018-05-03 12:36:49 -0500

    ifioma uk  sp

--- application/views/v_admin.php
+++ application/views/v_admin.php
@@ -229,7 +229,7 @@
           <?php } ?>
           $('.buttons-excel').empty();
           $('.buttons-print').empty();
-          $('.buttons-excel').append('<i class="fa fa-file-excel-o"></i>');
+          $('.buttons-excel').append('<i class="fa fa-download"></i>');
           $('.buttons-print').append('<i class="fa fa-print"></i>');
       });
     </script>
